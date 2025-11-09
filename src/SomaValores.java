81eefa21504ce3f39dba29a9cb943a2dd04dcc5c
Lucas Silva <lucas.silva.s1998@gmail.com>
2025-11-09 11:51:51 -0300

    Create SomaValores.java

---
+++ src/SomaValores.java
@@ -0,0 +1,11 @@
+public class SomaValores {
+    static void main(String[] args) {
+        int[] valores = {10, 20, 30, 40, 50};
+        int soma = 0;
+        for (int i = 0; i < valores.length; i++) {
+            soma += valores[i];
+        }
+
+        System.out.println("A soma total das receitas é " + soma);
+    }
+}
