1ce89419aaab670763d4d6f6b0984204927abdff
Lucas Silva <lucas.silva.s1998@gmail.com>
2025-11-12 19:01:52 -0300

    updates

---
+++ src/ContagemPulandoNúmeros.java
@@ -0,0 +1,18 @@
+void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
+    int numero;
+
+    System.out.print("Digite um numero: ");
+    numero = input.nextInt();
+
+    for (int i = 1; i <= numero; i++) {
+        if(i % 10 == 5) {
+            i += 1;
+        }
+
+        System.out.println(i + " ");
+    }
+
+    input.close();
+}
--- src/NumerosPositivosENegativos.java
+++ src/NumerosPositivosENegativos.java
@@ -1,7 +1,30 @@
 void main(String[] args) {
     Scanner input = new Scanner(System.in);
 
+    String entrada;
+    int totalDeNumerosPositivos = 0;
+    int totalDeNumerosNegativos = 0;
+    int numero = 0;
 
 
+    do {
+        System.out.println("Digite números para realizar a contagem de negativos e positivos (ou digite 'Fim' para encerrar.)");
+        entrada = input.nextLine();
+        entrada = entrada.toUpperCase();
+
+        if (entrada.equalsIgnoreCase("Fim")) {
+            break;
+        }
+        numero = Integer.parseInt(entrada);
+
+        if (numero > 0) {
+            totalDeNumerosPositivos += 1;
+        } else if (numero < 0) {
+            totalDeNumerosNegativos += 1;
+        }
+    } while(!entrada.equalsIgnoreCase("Fim"));
+
+    System.out.println("Total de números positivos: " +totalDeNumerosPositivos);
+    System.out.println("Total de números negativos: " +totalDeNumerosNegativos);
 
 }
